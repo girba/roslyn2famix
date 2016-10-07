def16be340d939b711fb624f22d091315f3c086f
Michael Turansky <m.turansky@gmail.com>
2016-10-07 16:58:14 +0200

    Added SymbolWalker to traverse Semantic Models.

---
+++ src/Roslyn2Famix/SymbolWalker.cs
@@ -0,0 +1,52 @@
+﻿namespace Roslyn2Famix
+{
+    using Famix.Language;
+    using Microsoft.CodeAnalysis;
+
+    public class SymbolWalker : SymbolVisitor
+    {
+        private Class rootClass = null;
+
+        public string ToFamixString()
+        {
+            return rootClass?.ToString();
+        }
+
+
+        public override void VisitNamespace(INamespaceSymbol symbol)
+        {
+            foreach (var childSymbol in symbol.GetMembers())
+            {
+                childSymbol.Accept(this);
+            }
+
+            base.VisitNamespace(symbol);
+        }
+
+        public override void VisitNamedType(INamedTypeSymbol symbol)
+        {
+            var className = symbol.Name;
+            this.rootClass = new Class(className);
+            
+            foreach(var childSymbol in symbol.GetMembers())
+            {
+                childSymbol.Accept(this);
+            }
+
+            base.VisitNamedType(symbol);
+        }
+
+        public override void VisitMethod(IMethodSymbol symbol)
+        {
+            if (!symbol.IsImplicitlyDeclared)
+            {
+                var methodName = symbol.Name;
+                var method = new Method(methodName);
+
+                this.rootClass?.Methods.Add(method);
+            }
+            
+            base.VisitMethod(symbol);
+        }
+    }
+}
