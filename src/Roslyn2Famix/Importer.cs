5a88bab9ddbc88e2a5c03f0c5933e46942e73443
Michael Turansky <m.turansky@gmail.com>
2016-10-08 11:57:17 +0200

    Added Importer class designed to import whole solutions from *.sln files.

---
+++ src/Roslyn2Famix/Importer.cs
@@ -0,0 +1,40 @@
+﻿namespace Roslyn2Famix
+{
+    using Famix;
+    using Microsoft.CodeAnalysis.MSBuild;
+
+    public class Importer
+    {
+        private readonly FamixTreeBuilder builder;
+        private readonly SymbolWalker walker;
+
+        public Importer()
+        {
+            this.builder = new FamixTreeBuilder();
+            this.walker = new SymbolWalker(builder);
+        }
+
+        public void Run(string solutionPath)
+        {
+            var workspace = MSBuildWorkspace.Create();
+
+            var solution = workspace.OpenSolutionAsync(solutionPath).Result;
+
+            builder.CreateSolution();
+
+            foreach (var project in solution.Projects)
+            {
+                builder.CreateProject(project.Name);
+
+                var compilation = project.GetCompilationAsync().Result;
+
+                walker.Visit(compilation.Assembly);
+            }
+        }
+
+        public string ExportToString()
+        {
+            return builder.ToFamixString();
+        }
+    }
+}
