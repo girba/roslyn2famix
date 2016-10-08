a40f16ecb813a1c204004c68a36c5ab16bb428ba
Michael Turansky <m.turansky@gmail.com>
2016-10-08 12:52:19 +0200

    Added Tests for Importer.

---
+++ src/Tests/ImporterTest.cs
@@ -0,0 +1,45 @@
+﻿namespace Tests
+{
+    using System;
+    using Roslyn2Famix;
+    using Xunit;
+    using System.IO;
+
+    /// <summary>
+    /// Tests the <see cref="Importer"/> public API.
+    /// </summary>
+    public class ImporterTest
+    {
+        private readonly string pathToTestSolution;
+
+        private readonly Importer importerUnderTest;
+
+        public ImporterTest()
+        {
+            const string solutionDirectoryPath = @"..\..\..\..\..\..\";
+            this.pathToTestSolution = Path.Combine(solutionDirectoryPath, "Roslyn2Famix.sln");
+
+            this.importerUnderTest = new Importer();
+        }
+
+        /// <summary>
+        /// Tests the Strategy <see cref="Importer.Run"/> method.
+        /// </summary>
+        [Fact]
+        public void Run_Test()
+        {
+            // Arrange
+            var expectedFamix = "(Class SimpleClassWithMethod)" + Environment.NewLine +
+                                "(Method SimpleMethod)" + Environment.NewLine +
+                                "(Class SimpleClassWithMethod end)" + Environment.NewLine;
+
+            // Act
+            importerUnderTest.Run(this.pathToTestSolution);
+
+            // Assert
+            var parsedFamix = importerUnderTest.ExportToString();
+
+            Assert.Equal(parsedFamix, expectedFamix);
+        }
+    }
+}
