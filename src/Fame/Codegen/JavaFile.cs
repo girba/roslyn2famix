f9a036ce570d8b602158e99517c2884666e7b051
Wolf-Dieter Hofmann <wolf-dieter.hofmann@gmx.de>
2017-09-19 22:07:51 +0200

    Added some classes to Codegen namespace

---
+++ src/Fame/Codegen/JavaFile.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.IO;
+
+namespace Fame.Codegen
+{
+	using System.Collections.Generic;
+	using System.Text;
+
+	public class JavaFile  // TODO: CsharpFile
+	{
+		// TODO: change names to match C# semantics (e.g. import = using)
+		private readonly HashSet<string> _imports;
+		private readonly string _myPackage;
+		private readonly string _name;
+		private string _superName;
+		private string _modelPackagename;
+		private string _modelClassname;
+
+		public JavaFile(string myPackage, string name)
+		{
+			_myPackage = myPackage;
+			_name = name;
+			ContentStream = new StringBuilder();
+			_imports = new HashSet<string>();
+		}
+
+		// TODO: Verify
+		public void AddImport(Type tee)
+		{
+			AddImport(tee.Assembly.GetName().Name, tee.Name);
+
+			//public <T> void addImport(Class<T> tee) {
+			//	this.addImport(tee.getPackage().getName(), tee.getSimpleName());
+			//}
+		}
+
+		public void AddImport(string aPackage, string className)
+		{
+			if (aPackage.Equals(_myPackage))
+			{
+				return;
+			}
+
+			if (aPackage.Equals("System"))  // TODO: C# - System.* ?
+			{
+				return;
+			}
+
+			_imports.Add(aPackage + "." + className);  // TODO: C# ?
+		}
+
+		public void AddSuperclass(string aPackage, string className)
+		{
+			if (className.Equals("object") && aPackage.Equals("System"))
+			{
+				return;
+			}
+
+			AddImport(aPackage, className);
+			_superName = className;
+		}
+
+		public void GenerateCode(StreamWriter stream)
+		{
+			Template template = Template.Get("Class");
+
+			template.Set("PACKAGE", _myPackage);
+
+			template.Set("AUTOGENCODE", "Automagically generated code");
+
+			template.Set("THISTYPE", _name);
+
+			template.Set("THISPACKAGE", _modelPackagename);
+
+			template.Set("THISNAME", _modelClassname);
+
+			template.Set("EXTENDS", _superName == null ? "" : "extends " + _superName);
+
+			template.Set("IMPORTS", Imports);
+
+			template.Set("FIELDS", "");
+
+			template.Set("METHODS", ContentStream.ToString());
+
+			stream.Write(template.Apply());
+		}
+
+		public StringBuilder ContentStream { get; }
+
+		public string Imports
+		{
+			get
+			{
+				StringBuilder stream = new StringBuilder();
+
+				foreach (string each in _imports)
+				{
+					stream.Append("import ").Append(each).Append(";\n");
+				}
+
+				return stream.ToString();
+			}
+		}
+
+		public string ModelClassname
+		{
+			get => _modelClassname;
+			set => _modelClassname = value;
+		}
+
+		public string ModelPackagename
+		{
+			get => _modelPackagename;
+			set => _modelPackagename = value;
+		}
+	}
+}
---
+++ src/Fame/Codegen/Template.cs
@@ -0,0 +1,113 @@
+﻿namespace Fame.Codegen
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Diagnostics;
+	using System.IO;
+	using System.Text;
+
+	public class Template
+	{
+		public static Template Get(string key)
+		{
+			Template template = AllTemplates[key];
+
+			Debug.Assert(template != null, key);
+
+			return template;
+		}
+
+		public static IDictionary<string, Template> AllTemplates
+		{
+			get
+			{
+				IDictionary<string, Template> map = new Dictionary<string, Template>();
+
+				using (var input = File.OpenRead("template.txt"))
+				{
+					using (var reader = new StreamReader(input))
+					{
+
+						string line = reader.ReadLine();
+						while (true)
+						{
+							if (line == null)
+							{
+								break;
+							}
+
+							Debug.Assert(line.StartsWith("%%%", StringComparison.Ordinal));
+
+							StringBuilder template = new StringBuilder();
+							string name = line.Substring(4).Trim();
+
+							while (true)
+							{
+								line = reader.ReadLine();
+								if (line == null || line.StartsWith("%%%", StringComparison.Ordinal))
+								{
+									break;
+								}
+
+								template.Append(line);
+								template.Append('\n');
+							}
+
+							map[name] = new Template(name, template.ToString());
+						}
+					}
+				}
+
+				return map;
+			}
+		}
+
+		public static void Main(params string[] argh)
+		{
+			Console.Write(AllTemplates);
+		}
+
+		private readonly string _template;
+		private readonly IDictionary<string, string> _values = new Dictionary<string, string>();
+
+		public Template(string name, string template)
+		{
+			_template = template;
+		}
+
+		public string Apply()
+		{
+			string result = _template;
+
+			foreach (string key in _values.Keys)
+			{
+				result = result.Replace("--" + key + "--", _values[key]);
+			}
+
+			return result;
+		}
+
+		public void Set(string key, string value)
+		{
+			_values[key] = value;
+		}
+
+		public Template All
+		{
+			set
+			{
+				foreach (var kv in value._values)
+				{
+					if (_values.ContainsKey(kv.Key))
+					{
+						_values[kv.Key] = kv.Value;
+					}
+					else
+					{
+						_values.Add(kv.Key, kv.Value);
+					}
+				}
+			}
+		}
+	}
+}
